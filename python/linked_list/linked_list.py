322b2f4aa2cc45ac7f6be82159da9c0bc5070821
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-11-07 02:19:24 +0200

    finish

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,67 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next= None
 class LinkedList:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head= None
 
-    def some_method(self):
-        # method body here
-        pass
+    def append(self,value):
+        node= Node(value)
+        if self.head is None:
+            self.head=node
+        else:
+            current= self.head
+            while current.next != None:
+                current = current.next
+            current.next = node
+
+    def insert(self,value):
+        node= Node(value)
+        
+        if self.head is None:
+            self.head=node
+        else:
+            newNode= node
+            newNode.next = self.head
+            self.head= newNode
+         
+    def recInclude(self,node,key):
+        if node == None:
+            return False
+        if node.value == key:
+            return True 
+        return self.recInclude(node.next,key)
+
+    def include(self,key):
+        current = self.head
+        while current != None:
+            if current.value == key:
+                return True
+           
+            current = current.next 
+        return False
+
+
+    def __str__(self):
+        output = "head -> "
+        if self.head is None:
+            output += None
+        else:
+            current = self.head
+            while(current) :
+                output += f"{current.value} -> " 
+                current= current.next   
+            output += "None"
+            return output
+ll=LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+
+ll.insert(0)
+
+if __name__== "__main__":
+    print(ll)
+    print(ll.include(20))
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,33 @@
-from linked_list.linked_list import LinkedList
+# from linked_list import __version__
+from linked_list.linked_list import LinkedList,Node
+import pytest
+
+# def test_version():
+#     assert __version__ == "0.1.0"
 
 
-def test_import():
-    assert LinkedList
+def test_emty_linked_list():
+    ll=LinkedList()
+    expected=None
+    actual=ll.head
+    assert expected==actual
+
+def test_appen3_values(ll):
+  
+    expected = "head -> 5 -> hello -> 1.57 -> None" 
+    actual = ll.__str__()
+    assert expected == actual
+
+def test_append_to_existing_ll(ll):
+    ll.append(True)
+    expected="head -> 5 -> hello -> 1.57 -> True -> None"
+    actual = ll.__str__()
+    assert expected == actual
+
+@pytest.fixture
+def ll():
+    ll=LinkedList()
+    ll.append(5) 
+    ll.append('hello') 
+    ll.append(1.57)
+    return ll 
